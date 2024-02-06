dcdf2be9bf5afd5cfa866ee1cad3fbce80dc627c
absarahmed59 <68219741+absarahmed59@users.noreply.github.com>
2024-02-06 08:04:42 +0000

    eyak

--- .ccls-cache/@home@runner@nothing/new.cpp
+++ .ccls-cache/@home@runner@nothing/new.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
-
 void Prime() {
-    for (int i = 2; i < 1000; i++) {
-        bool isPrime = true;
-        for (int j = 2; j*j <= i; j++) {
-            if (i % j == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            cout << i << " ";
-        }
+  for (int i = 2; i < 1000; i++) {
+    bool isPrime = true;
+    for (int j = 2; j * j <= i; j++) {
+      if (i % j == 0) {
+        isPrime = false;
+        break;
+      }
     }
-    return;
+    if (isPrime) {
+      cout << i << " ";
+    }
+  }
+  return;
 }
