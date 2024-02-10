8138d6f32a9f0a8110b66254a75cf3512c5b9817
absarahmed59 <68219741+absarahmed59@users.noreply.github.com>
2024-02-10 06:30:08 +0000

    ..

---
+++ .ccls-cache/@home@runner@nothing/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+class Birthday {
+public:
+  int day;
+  int month;
+  int year;
+
+  Birthday(int d, int m, int y) {
+    day = d;
+    month = m;
+    year = y;
+  }
+
+  void printDateOfBirth() {
+    cout << "Date of Birth: " << day << "/" << month << "/" << year << endl;
+  }
+};
+
+int main() {
+  Birthday b(11, 02, 2004);
+  b.printDateOfBirth();
+  return 0;
+}
+Nothing to be added
