ba62ea0c48c8587f80bc9c901d264fb90c7adb84
absarahmed59 <68219741+absarahmed59@users.noreply.github.com>
2024-02-18 03:47:23 +0000

    Destructors

---
+++ .ccls-cache/@home@runner@nothing/des.cpp
@@ -0,0 +1,23 @@
+class Class1 {
+public:
+    Class1() {
+        cout << "Constructor for Class1" << endl;
+    }
+
+    ~Class1() {
+        cout << "Destructor for Class1" << endl;
+    }
+};
+
+class Class2 {
+public:
+    Class1 obj;
+
+    Class2() {
+        cout << "Constructor for Class2" << endl;
+    }
+
+    ~Class2() {
+        cout << "Destructor for Class2" << endl;
+    }
+};
