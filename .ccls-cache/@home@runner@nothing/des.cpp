f8d0c2ede0cb0f4d88097afcf26b1562a9ca45e4
absarahmed59 <68219741+absarahmed59@users.noreply.github.com>
2024-02-18 03:51:49 +0000

    Commented

--- .ccls-cache/@home@runner@nothing/des.cpp
+++ .ccls-cache/@home@runner@nothing/des.cpp
@@ -1,23 +1,16 @@
 class Class1 {
 public:
-    Class1() {
-        cout << "Constructor for Class1" << endl;
-    }
+  Class1() { cout << "Constructor for Class1" << endl; }
 
-    ~Class1() {
-        cout << "Destructor for Class1" << endl;
-    }
+  ~Class1() { cout << "Destructor for Class1" << endl; }
 };
 
 class Class2 {
 public:
-    Class1 obj;
+  Class1 obj;
 
-    Class2() {
-        cout << "Constructor for Class2" << endl;
-    }
+  Class2() { cout << "Constructor for Class2" << endl; }
 
-    ~Class2() {
-        cout << "Destructor for Class2" << endl;
-    }
+  ~Class2() { cout << "Destructor for Class2" << endl; }
 };
+// commented
