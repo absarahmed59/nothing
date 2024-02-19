a8e2de2013abf23cd612fc8936fa5f7dd660417c
absarahmed59 <68219741+absarahmed59@users.noreply.github.com>
2024-02-19 05:56:31 +0000

    Caches

---
+++ .ccls-cache/@home@runner@nothing/primes.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+bool is_prime(int num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= sqrt(num); i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void print_primes_from_input(int n) {
+  int count = 0;
+  int num = n + 1;
+  while (count < 10) {
+    if (is_prime(num)) {
+      cout << num << " ";
+      count++;
+    }
+    num++;
+  }
+}
+
+int Prime() {
+  int input_num;
+  cout << "Enter a number: ";
+  cin >> input_num;
+
+  cout << "First 10 primes from " << input_num << " are: ";
+  print_primes_from_input(input_num);
+
+  return 0;
+}
